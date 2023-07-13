447ea5af2f88479ad08801850591214a7c76393d
santosha-2001 <139257295+santosha-2001@users.noreply.github.com>
2023-07-13 18:38:45 +0000

    added about, footer

---
+++ drone.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+@app.route("/")
+def project():
+  return render_template('home.html')
+if __name__ == "__main__":
+  app.run('0.0.0',debug=True)
+  
